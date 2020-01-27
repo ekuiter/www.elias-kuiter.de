fb9f63f2d7c71c211cc855c6b8240594bfd4a966
Elias Kuiter <info@elias-kuiter.de>
2020-01-27 18:26:58 +0100

    Changed scrolling behavior

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,23 +1,7 @@
 export const onInitialClientRender = () => {
-  let i = 0;
-
-  window.setTimeout(function scrollToAnchor() {
-    if (window.scrollTo) {
-      i++;
-      const element = window.location.hash && document.querySelector(window.location.hash);
-      const scrollPosition = parseInt(window.localStorage.getItem('scrollPosition'), 10);
-      const y = element
-        ? element.offsetTop
-        : !isNaN(scrollPosition)
-          ? scrollPosition
-          : 0;
-      window.scrollTo(0, y);
-      if (window.location.hash && !element && i < 10)
-        window.setTimeout(scrollToAnchor, 100);
-    }
+  window.setTimeout(function() {
+    const element = window.location.hash && document.querySelector(window.location.hash);
+    if (window.scrollTo && element)
+      window.scrollTo(0, element.offsetTop);
   }, 100);
-
-  window.addEventListener('beforeunload', () => {
-    window.localStorage.setItem('scrollPosition', window.pageYOffset);
-  });
 }
